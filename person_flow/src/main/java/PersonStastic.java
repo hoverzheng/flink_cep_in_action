23da57f688b41071d456455161039b437554d39e
Xiaohong Zheng <xiaohong@fintec.ai>
2020-02-01 21:37:35 +0800

    add person flow

---
+++ person_flow/src/main/java/PersonStastic.java
@@ -0,0 +1,43 @@
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.api.common.functions.FilterFunction;
+
+
+public class PersonStastic {
+
+    public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env =
+                StreamExecutionEnvironment.getExecutionEnvironment();
+
+        DataStream<Person> flintstones = env.fromElements(
+                new Person("Fred", 35),
+                new Person("Wilma", 35),
+                new Person("Pebbles", 2));
+
+        DataStream<Person> adults = flintstones.filter(new FilterFunction<Person>() {
+            public boolean filter(Person person) throws Exception {
+                return person.age >= 18;
+            }
+        });
+
+        adults.print();
+
+        env.execute();
+    }
+
+    public static class Person {
+        public String name;
+        public Integer age;
+        public Person() {};
+
+        public Person(String name, Integer age) {
+            this.name = name;
+            this.age = age;
+        };
+
+        public String toString() {
+            return this.name.toString() + ": age " + this.age.toString();
+        };
+    }
+}
